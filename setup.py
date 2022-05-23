ecff52eed686c89cd7f3c62f678da535b89cbce2
Ketan Agrawal <agrawalk@stanford.edu>
2022-05-23 15:29:08 -0700

    added dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(include=['neuralDecoder']),
     install_requires=[
-        'tensorflow-gpu==2.7.0',
+        'tensorflow==2.7.0'
         'hydra-core==1.1.0',
         'hydra-submitit-launcher==1.1.5',
         'transformers==4.12.5',
@@ -15,6 +15,8 @@ setup(
         'jupyterlab',
         'ipywidgets',
         'tqdm',
+        'wandb',
+        'conformer_tf'
         'g2p_en==2.0.0'
     ]
 )
