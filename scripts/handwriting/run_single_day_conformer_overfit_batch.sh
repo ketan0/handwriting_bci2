1963acdde3ec8a97fba85cec6c626a2b4b6236f2
Ketan Agrawal <agrawalk@stanford.edu>
2022-05-29 18:20:39 -0700

    rename

--- scripts/handwriting/run_single_day_conformer_overfit_batch.sh
+++ scripts/handwriting/run_single_day_conformer_overfit_batch.sh
@@ -3,7 +3,8 @@
 python -m neuralDecoder.main \
     model=conformer \
     lossType=rnnt \
-    dataset=handwriting_all_days \
+    dataset=handwriting_single_day \
+    dataset.subsetSize=2 \
     batchSize=2 \
     dataset.syntheticMixingRate=0 \
     outputDir=/Users/ketanagrawal/CS224s/run_all_days_conformer_output
