f9cc532070a38048c8e1baf7717fed0150804513
Ketan Agrawal <agrawalk@stanford.edu>
2022-05-30 02:33:25 -0700

    bump tf version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(include=['neuralDecoder']),
     install_requires=[
-        'tensorflow==2.7.0',
+        'tensorflow~=2.9.0',
         'hydra-core==1.1.0',
         'hydra-submitit-launcher==1.1.5',
         'transformers==4.12.5',
